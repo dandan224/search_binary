cc3e71a392ff6b33acb67e4c1bb87f6c9d9c44a2
dandan224 <43757729+dandan224@users.noreply.github.com>
2019-08-12 14:08:23 -0700

    Create Search_In_Bitonic_Array.py
    Search for a target number in a bitonic array, return the index of the target number if found in the array, or return -1.
    
    A bitonic array is a combination of two sequence: the first sequence is a monotonically increasing one and the second sequence is a monotonically decreasing one.
    
    Assumptions:
    
    The array is not null.
    Examples:
    
    array = {1, 4, 7, 11, 6, 2, -3, -8}, target = 2, return 5.

---
+++ Search_In_Bitonic_Array.py
@@ -0,0 +1,53 @@
+class Solution(object):
+  def search(self, array, target):
+    """
+    input: int[] array, int target
+    return: int
+    """
+    # write your solution here
+    if not array or not target:
+      return -1
+    index = self.findMax(array)
+    if array[index] == target:
+      return index
+    elif array[index] < target:
+      return -1
+    else:
+      left1, left2 = 0, index + 1
+      right1, right2 = index -1, len(array) - 1
+      while left1 <= right1:
+        mid1 = int((left1 + right1)/2)
+        if array[mid1] < target:
+          left = mid1 + 1
+        elif array[mid1] > target:
+          right = mid1 - 1
+        else:
+          return mid1
+      while left2 <= right2:
+        mid2 = int((left2 + right2)/2)
+        if array[mid2] < target:
+          right = mid2 - 1    
+        elif array[mid2] > target:
+          left = mid2 + 1
+        else:
+          return mid2
+      return -1
+
+      
+
+
+
+
+
+  def findMax(self, array):
+    if not array or len(array)==0:
+      return array
+    left, right = 0, len(array) - 1
+    while left < right:
+      mid = int((left + right)/2)
+      if array[mid] < array[mid + 1]:
+        left = mid + 1
+      else:
+        right = mid
+    return right
+  
